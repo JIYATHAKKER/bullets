623c8c1865fc8a3683336bc609f078208f5fc184
JIYATHAKKER <64413901+JIYATHAKKER@users.noreply.github.com>
2020-06-01 23:07:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var thickness,wall;
+var  speed,weight,bullet;
+var damage;
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  bullet=createSprite(50,200,50,50);
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+  bullet.velocityX=speed;
+  thickness=random(22,83);
+
+
+}
+
+function draw() {
+  background(0);
+  if(bullet.width/2+wall.width/2>wall.x-bullet.x){
+    bullet.velocityX=0;
+    damage=0.5*bulletWeight*bulletSpeed*bulletSpeed
+      if(damage<100){
+      bullet.shapeColor=color(0,255,0);
+    }
+      if(damage>=100&&damage<=180){
+      bullet.shapeColor=color(230,230,0);
+    }
+    if(damage>180){
+      wall.shapeColor=color(255,0,0);
+    }
+  
+  } 
+  drawSprites();
+}
